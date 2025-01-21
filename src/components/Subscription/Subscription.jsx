626762e0c2c395035facaf63f2817b7418e5a483
Sachin Vardhan <vardhansachin08@gmail.com>
2025-01-21 12:03:50 +0530

    Subs

--- src/components/Subscription/Subscription.jsx
+++ src/components/Subscription/Subscription.jsx
@@ -118,7 +118,7 @@ const Subscription = () => {
 
       // Initialize Razorpay
       const options = {
-        key: "rzp_test_6coJFlp4ji11OO",
+        key: "p_live_yd8codgpJRRZ29",
         amount: plan.price,
         currency: "INR",
         name: "NOTO Study",
