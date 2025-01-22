01eb43d9c9a120746d1b63fb07b44b7346f8966e
Sachin Vardhan <vardhansachin08@gmail.com>
2025-01-22 17:07:59 +0530

    Added Forget Pass

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import HomePage from './components/Pages/HomePage';
 import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
-import Subscription from './components/Subscription/Subscription';
-import HomePage from './components/Pages/HomePage';
-import Profile from './components/Auth/Profile';
+import Subscription from './components/Pages/Subscription';
+import Profile from './components/Pages/Profile';
 import Notes from './components/Pages/Notes';
 import FAQs from './components/Pages/FAQs';
 import ContactUs from './components/Pages/ContactUs';
-import AboutUs from './components/Verify Pages/AboutUs';
+import AboutUs from './components/Pages/AboutUs';
 import PrivacyPolicy from './components/Verify Pages/PrivacyPolicy';
 import TandQ from './components/Verify Pages/T&Q';
 import RefundPolicy from './components/Verify Pages/RefundPolicy';
@@ -21,14 +22,14 @@ const App = () => (
     <Route path="/subscription" element={<Subscription />} />
     <Route path="/homepage" element={<HomePage/>} />
     <Route path="/profile" element={<Profile />} />
-    <Route path='/notes' element={<Notes />} />
-    <Route path='/faqs' element={<FAQs />} />
-    <Route path='/contactus' element={<ContactUs />} />
-    <Route path='/about' element={<AboutUs />} />
-    <Route path='/privacy' element={<PrivacyPolicy />} />
-    <Route path='/terms' element={<TandQ />} />
-    <Route path='/refund' element={<RefundPolicy />} />
-    <Route path='/forgot-password' element={<ForgetPass />} />
+    <Route path="/notes" element={<Notes />} />
+    <Route path="/faqs" element={<FAQs />} />
+    <Route path="/contactus" element={<ContactUs />} />
+    <Route path="/about" element={<AboutUs />} />
+    <Route path="/privacy" element={<PrivacyPolicy />} />
+    <Route path="/terms" element={<TandQ />} />
+    <Route path="/refund" element={<RefundPolicy />} />
+    <Route path="/forgot-password" element={<ForgetPass />} />
   </Routes>
 );
 
